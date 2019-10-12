ce8aa3b128fcc838e302a15b34b86cada8e8aa3f
Ozi <voznesenskiy.evgeniy@mail.ru>
2019-10-12 17:02:26 +0300

    v1

--- src/main/java/com/example/copter_vision/MainController.java
+++ src/main/java/com/example/copter_vision/MainController.java
@@ -1,24 +1,50 @@
 package com.example.copter_vision;
 
 import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
+
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @PostMapping("/example")
+    public ResponseEntity postExample(@RequestBody String body) throws IOException {
+        File file = new File("C:/Users/Ozi/ideaProjects/copter_vision/src/main/resources/templates/video.html");
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(body);
+        bufferedWriter.close();
+        return ResponseEntity.ok("success");
+
     }
 
-    @PostMapping("/index")
+    @GetMapping("/example")
     @ResponseBody
-    public String getVideo(@RequestBody String http) {
-        System.out.println(http);
-        return http;
+    public String getExample() {
+        File file = new File("C:/Users/Ozi/ideaProjects/copter_vision/src/main/resources/templates/video.html");
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = null;
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+            bufferedReader.close();
+            return stringBuffer.toString() != null ? stringBuffer.toString() : "<html><body>none</body></html>";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "<html><body>none</body></html>";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "<html><body>none</body></html>";
+        }
+
     }
 
 }
